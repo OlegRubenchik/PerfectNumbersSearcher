f39fee42453c59fc1a552b7f0aace237c32f7b49
Oleg Rubenchik <oleg.rubenchik25@gmail.com>
2024-03-12 19:21:43 +0200

    Added some comments

--- Perfect numbers/perf_nums.cpp
+++ Perfect numbers/perf_nums.cpp
@@ -1,8 +1,23 @@
+/** /file perf_nums.cpp
+* /brief Search for perfect number.
+* /version 1.0
+* /date 12/03/2024
+* /author Oleg Rubenchik
+*/
+
 #include <iostream>
 using namespace std;
 
-void searchForPerfNums(int UpBorder);
 
+//	PROTOTYPES
+void searchForPerfNums(const int UpBorder);
+
+
+/**
+* Function <code>main</code> Interface.
+* <BR>
+* @return Returns 0.
+*/
 int main() {
 	int num;
 	cout << "Enter till what number you want to search: ";
@@ -11,7 +26,12 @@ int main() {
 	return 0;
 	
 }
-void searchForPerfNums(int UpBorder) {
+/**
+* Function <code>searchForPerfNums</code> Searches for perfect numbers.
+* <BR>
+* @param UpBorder Upper border of the range of numbers.
+*/
+void searchForPerfNums(const int UpBorder) {
 	bool flag = false;
 	if (UpBorder >= 100000) flag = true;
 	for (int i = 2; i <= UpBorder; i = i + 2) {
